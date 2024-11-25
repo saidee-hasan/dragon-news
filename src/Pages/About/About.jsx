00e299644c197c5883bb2b73ca423a061910e9b9
saideeWD <mdsaideehasan@gmail.com>
2024-11-25 20:58:30 +0600

    about section problem

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../ShereAble/Header';
 import Navbar from '../ShereAble/Navbar';
-import { Helmet } from 'react-helmet-async';
+
 
 const About = () => {
   return (
